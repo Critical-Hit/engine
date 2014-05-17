c73ffdd23b71e2d7aa797d19797325e726649de3
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2014-05-17 17:19:24 -0600

    GL/Glew on Linux? Need to test on a standard OS

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,23 +1,26 @@
-//
+
 //  main.cpp
 //
 
 #include <iostream>
 #include <string>
+
 #include <unordered_set>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <GL/glew.h>
 
 using namespace std;
 
 int main(int argc, const char * argv[])
 {
-    auto test = [] () { printf("Hello World\n"); };
-    test();
-    
-    unordered_set<std::string> first ({"red","green","blue"});
-    for(const string& x: first) cout << " " << x;
-    cout << endl;
+	sf::Window window(sf::VideoMode(640, 480, 32), "SFML Window with OpenGL");
+	GLenum err = glewInit();
+	if (err != GLEW_OK)
+		printf("There was an error initializing glew.");
+	glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	getchar();
-
-    return 0;
+	return 0;
 }
 
