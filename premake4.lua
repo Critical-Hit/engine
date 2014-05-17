c97e26831536f2d4fc3c5062b35f55e06121e3cf
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2014-05-17 02:19:58 -0600

    initial commit

---
+++ premake4.lua
@@ -0,0 +1,25 @@
+solution "Engine"
+	configurations {"Debug", "Release"}
+	platforms ("native", "Universal")
+	configuration "Debug"
+		targetdir ("bin/debug")
+	configuration "Release"
+		targetdir ("bin/release")
+	configuration {"linux", "gmake" }
+		buildoptions {"-std=c++11"}
+	configuration {"macosx", "xcode3"}
+		-- todo force c++11
+	configuration {"windows", "vs2010"}
+		-- todo force c++11
+
+project "Core"
+	kind "ConsoleApp"
+	language "C++"
+	files {
+		"src/core/*.h",
+		"src/core/*.cpp"
+	}
+	flags {
+		"ExtraWarnings"
+	}
+
