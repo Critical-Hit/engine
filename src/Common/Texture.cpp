72d62f259d469c35231bf5777f4c0fa5dabc2cf7
Richard Blaylock <blay4480@Stitch.local>
2014-07-22 20:36:44 -0600

    Test request on Windows and linux. I put the text image in a folder outside of the bin/debug folder and it loaded. I want to see if it loads on linux and windows with the same filepath.

--- src/Common/Texture.cpp
+++ src/Common/Texture.cpp
@@ -5,7 +5,7 @@ Texture::Texture()
 {
     this->textureID =  SOIL_load_OGL_texture
     (
-     "meeseeks.png",
+     "../../meeseeks.png",
      SOIL_LOAD_AUTO,
      SOIL_CREATE_NEW_ID,
      SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT
