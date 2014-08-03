7fbd677c967ba62e14ac1112728982cafece497b
David Setser <davidjsetser@gmail.com>
2014-08-03 10:19:46 -0700

    Fixes for tabs, line endings, and a few issues found through code review.

--- src/Common/Camera.cpp
+++ src/Common/Camera.cpp
@@ -3,105 +3,107 @@
 
 Camera::Camera() : x(0.0f), y(0.0f), width(2.0f), height(2.0f), reverseY(false)
 {
-	this->halfWidth = width / 2.0f;
-	this->halfHeight = height / 2.0f;
+    this->halfWidth = width / 2.0f;
+    this->halfHeight = height / 2.0f;
 }
 
 Camera::Camera(float x, float y, float width, float height) : x(x), y(y), width(width), height(height), reverseY(false)
 {
-	this->halfWidth = width / 2.0f;
-	this->halfHeight = height / 2.0f;
+    this->halfWidth = width / 2.0f;
+    this->halfHeight = height / 2.0f;
 }
 
 void Camera::MoveTo(float x, float y)
 {
-	this->x = x;
-	this->y = y;
+    this->x = x;
+    this->y = y;
 }
 
 void Camera::MoveBy(float dx, float dy)
 {
-	this->x += dx;
-	this->y += dy;
+    this->x += dx;
+    this->y += dy;
 }
 
 void Camera::SetDimensions(float width, float height)
 {
-	this->halfWidth = width / 2.0f;
-	this->halfHeight = height / 2.0f;
+    this->width = width;
+    this->halfWidth = width / 2.0f;
+    this->height = height;
+    this->halfHeight = height / 2.0f;
 }
 
 float Camera::getX()
 {
-	return this->x;
+    return this->x;
 }
 
 float Camera::getY()
 {
-	return this->y;
+    return this->y;
 }
 
 float Camera::getWidth()
 {
-	return this->width;
+    return this->width;
 }
 
 float Camera::getHeight()
 {
-	return this->height;
+    return this->height;
 }
 
 float Camera::GetLeft()
 {
-	return this->x - this->halfWidth;
+    return this->x - this->halfWidth;
 }
 
 float Camera::GetRight()
 {
-	return this->x + this->halfWidth;
+    return this->x + this->halfWidth;
 }
 
 float Camera::GetTop()
 {
-	if (reverseY)
-	{
-		return this->y - this->halfHeight;
-	}
-	else
-	{
-		return this->y + this->halfHeight;
-	}
+    if (this->reverseY)
+    {
+        return this->y - this->halfHeight;
+    }
+    else
+    {
+        return this->y + this->halfHeight;
+    }
 }
 
 float Camera::GetBottom()
 {
-	if (reverseY)
-	{
-		return this->y + this->halfHeight;
-	}
-	else
-	{
-		return this->y - this->halfHeight;
-	}
+    if (this->reverseY)
+    {
+        return this->y + this->halfHeight;
+    }
+    else
+    {
+        return this->y - this->halfHeight;
+    }
 }
 
 
 std::string Camera::ToString()
 {
-	return "X: " + std::to_string(this->x) + " Y: " + std::to_string(this->y) + " Width: " + std::to_string(this->width) + " Height: " + std::to_string(this->height);
+    return "X: " + std::to_string(this->x) + " Y: " + std::to_string(this->y) + " Width: " + std::to_string(this->width) + " Height: " + std::to_string(this->height);
 }
 
 std::string Camera::ToStringLRTB()
 {
-	return "Left: " + std::to_string(this->GetLeft()) + " Right: " + std::to_string(this->GetRight()) + " Top: " + std::to_string(this->GetTop()) + " Bottom: " + std::to_string(this->GetBottom());
+    return "Left: " + std::to_string(this->GetLeft()) + " Right: " + std::to_string(this->GetRight()) + " Top: " + std::to_string(this->GetTop()) + " Bottom: " + std::to_string(this->GetBottom());
 }
 
 void Camera::SetReverseY(bool reverseY)
 {
-	this->reverseY = reverseY;
+    this->reverseY = reverseY;
 }
 
 Camera::~Camera()
 {
-
+    
 }
--- src/Common/GraphicsManager.cpp
+++ src/Common/GraphicsManager.cpp
@@ -7,75 +7,75 @@
 
 GraphicsManager::GraphicsManager() : clearColor(0.0f, 0.0f, 0.0f, 1.0f), camera()
 {
-
+    
 }
 
 GraphicsManager::~GraphicsManager()
 {
-
+    
 }
 
 Color GraphicsManager::GetClearColor()
 {
-	return this->clearColor;
+    return this->clearColor;
 }
 
 void GraphicsManager::SetClearColor(Color clearColor)
 {
-	this->clearColor = clearColor;
+    this->clearColor = clearColor;
 }
 
 void GraphicsManager::RegisterSprite(Sprite* sprite)
 {
-	this->registeredSpritesMutex.lock();
-	int registeredCountBeforeAdd = (int)this->registeredSprites.size();
-	this->registeredSprites.insert(sprite);
-	if ((registeredCountBeforeAdd + 1) != (int)this->registeredSprites.size())
-	{
-		throw new std::invalid_argument("A sprite was registered that was already registered.");
-	}
-	this->registeredSpritesMutex.unlock();
+    this->registeredSpritesMutex.lock();
+    int registeredCountBeforeAdd = (int)this->registeredSprites.size();
+    this->registeredSprites.insert(sprite);
+    if ((registeredCountBeforeAdd + 1) != (int)this->registeredSprites.size())
+    {
+        throw new std::invalid_argument("A sprite was registered that was already registered.");
+    }
+    this->registeredSpritesMutex.unlock();
 }
 
 void GraphicsManager::UnRegisterSprite(Sprite* sprite)
 {
-	this->registeredSpritesMutex.lock();
-	int registeredCountBeforeAdd = (int)this->registeredSprites.size();
-	this->registeredSprites.erase(sprite);
-	if ((registeredCountBeforeAdd - 1) != (int)this->registeredSprites.size())
-	{
-		throw new std::invalid_argument("A sprite was unregistered that wasn't registered.");
-	}
-	this->registeredSpritesMutex.unlock();
+    this->registeredSpritesMutex.lock();
+    int registeredCountBeforeAdd = (int)this->registeredSprites.size();
+    this->registeredSprites.erase(sprite);
+    if ((registeredCountBeforeAdd - 1) != (int)this->registeredSprites.size())
+    {
+        throw new std::invalid_argument("A sprite was unregistered that wasn't registered.");
+    }
+    this->registeredSpritesMutex.unlock();
 }
 
 int GraphicsManager::GetSpriteCount()
 {
-	return (int)this->registeredSprites.size();
+    return (int)this->registeredSprites.size();
 }
 
 Camera* GraphicsManager::GetCamera()
 {
-	return &(this->camera);
+    return &(this->camera);
 }
 
 void GraphicsManager::PrepareToAddSprites()
 {
-	this->registeredSpritesMutex.lock();
-	this->spriteIterator = this->registeredSprites.begin();
+    this->registeredSpritesMutex.lock();
+    this->spriteIterator = this->registeredSprites.begin();
 }
 
 bool GraphicsManager::AddSpriteToVCIBuffer(float* vertexBuffer, float* colorBuffer, unsigned short* indexBuffer, unsigned short dataStartIndex)
 {
-	if (this->spriteIterator == this->registeredSprites.end())
-	{
-		this->registeredSpritesMutex.unlock();
-		return false;
-	}
-	Sprite* sprite = *(this->spriteIterator);
-	sprite->PutGLVertexInfo(vertexBuffer); // 16 = 4 vertices * 4 coordinates
-	sprite->PutGLColorInfo(colorBuffer); // 16 = 4 vertices * 4 channels
-	sprite->PutGLIndexInfo(indexBuffer, dataStartIndex); // 6 = 2 triangles * 3 coordinates
-	this->spriteIterator++;
-	return true;
+    if (this->spriteIterator == this->registeredSprites.end())
+    {
+        this->registeredSpritesMutex.unlock();
+        return false;
+    }
+    Sprite* sprite = *(this->spriteIterator);
+    sprite->PutGLVertexInfo(vertexBuffer); // 16 = 4 vertices * 4 coordinates
+    sprite->PutGLColorInfo(colorBuffer); // 16 = 4 vertices * 4 channels
+    sprite->PutGLIndexInfo(indexBuffer, dataStartIndex); // 6 = 2 triangles * 3 coordinates
+    this->spriteIterator++;
+    return true;
 }
