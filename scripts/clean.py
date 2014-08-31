51cea59ce580a7ca95d0bd0e02682163b5a05e73
dsetser <danielsetser@gmail.com>
2014-08-31 11:41:45 -0700

    Fixed script to use python calls for removing files and folders

--- scripts/clean.py
+++ scripts/clean.py
@@ -1,21 +1,21 @@
 from subprocess import call
+import os
 import os.path
-import platform
-import sys
+import glob
+import shutil
 
 # Add any additional files you wish to clean here
-FILES_TO_REMOVE = ["obj", "*.suo", "*.sdf"]
+FILES_TO_REMOVE = ["*.suo", "*.sdf"]
+FOLDERS_TO_REMOVE = ["obj"]
 
 
-def remove_file(filename, windows):
-    try:
-        if windows:
-            call(["del", "/F", "/Q", filename])
-        else:
-            call(["rm", "-rf", filename])
-    except:
-        pass
+def remove_file(filename):
+    for f in glob.glob(filename):
+        os.remove(f)
 
+def remove_folder(folder):
+    for f in glob.glob(folder):
+        shutil.rmtree(f)
 
 def main():
     print("Cleaning Directory...")
@@ -25,7 +25,9 @@ def main():
         except:
             pass
     for file in FILES_TO_REMOVE:
-        remove_file(file, sys.argv[1] == "--windows")
+        remove_file(file)
+    for folder in FOLDERS_TO_REMOVE:
+        remove_folder(folder)
 
 if __name__ == "__main__":
     main()
